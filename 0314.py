5aefa65b2c8a537b998925ea7ec82573ab336890
Ryan <ryan970204@gmail.com>
2018-04-11 20:25:27 +0800

    bom

--- 0314.py
+++ 0314.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+
+screen_size = [700, 500]
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -30,6 +33,8 @@ class Player:
     def move(self):
         pos = pygame.mouse.get_pos()
         self.y = pos[1]  
+        self.x2 = self.x + 5
+        self.y2 = self.y + 15
 
 
 class Box:
@@ -38,24 +43,31 @@ class Box:
         self.y = y
         self.w = random.randint(10, 100)
         self.h = random.randint(10, 50)
-
+        self.x2 = self.x + self.w
+        self.y2 = self.y + self.h
         self.speed = speed
 
     def draw(self):
         pygame.draw.rect(screen, BLACK, [self.x, self.y, self.w, self.h], 0)
-    def move(self):      
+
+    def move(self):
         self.x -= self.speed
+        self.x2 = self.x + self.w
+        self.y2 = self.y + self.h
 
 def calc_score(score):
     return 'Score:' + str(score)
 
 def check(player, box):
-    for pb in boxes:
-        if player.x > box.x and player.x2 < box.x2 and player.y > box.y and player.y2 < box.y2:
-            return True
+    top_left = player.x <= box.x <= player.x2 and player.y <= box.y <= player.y2
+    bottom_right = player.x <= box.x2 <= player.x2 and player.y <= box.y2 <= player.y2
+    if top_left or bottom_right:
+        return True
     return False
+
 pygame.init()
-screen = pygame.display.set_mode([700, 500], pygame.RESIZABLE)
+
+screen = pygame.display.set_mode(screen_size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
 pygame.display.set_caption("My Game")
 done = False
 clock = pygame.time.Clock()
@@ -69,12 +81,23 @@ z = 0
 player = Player(0, 0)
 
 while not done:
+    # event 事件 (鍵盤敲擊, 滑鼠移動, 滑鼠按鍵..)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        # 畫面重新調整大小
+        if event.type == pygame.VIDEORESIZE:
+            # The main code that resizes the window:
+            # (recreate the window with the new size)
+            screen_size = (event.w, event.h)
+            surface = pygame.display.set_mode(screen_size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+            # 背景圖 放大
+            screen.blit(pygame.transform.scale(background_image, screen_size), (0, 0))
+            pygame.display.flip()
 
 
-    screen.blit(background_image, background_position)
+    screen.blit(pygame.transform.scale(background_image, screen_size), (0, 0)) # 把背景圖畫出來
+    # screen.blit(background_image, background_position)
     
     score_c += 1
     if score_c > 20:
@@ -102,6 +125,12 @@ while not done:
         box.move()
     player.move()
     player.draw()
+
+    for box in boxes:
+        重疊 = check(player, box)
+        if 重疊: 
+            print('失敗')
+
     pygame.display.flip() 
     clock.tick(60)
 pygame.quit()
