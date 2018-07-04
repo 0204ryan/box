56c66f9238e827288df58497bfe9e8a12e5dc240
Ryan <ryan970204@gmail.com>
2018-07-04 20:29:06 +0800

    can package as exe

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+
+from cx_Freeze import setup, Executable
+
+build_exe_options = {'include_files': ['piano.mp3', 'bg0.jpg', 'bg1.jpg', 'heli.png', 'wt014.ttf'], 'packages':['pygame']}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executable = Executable('main.py', base=base, targetName='方塊戰爭.exe')
+
+setup(
+        name = "box 1:0",
+        version = "1.0",
+        description = "a pygame game",
+        options = {'build_exe': build_exe_options},
+        executables = [executable])
